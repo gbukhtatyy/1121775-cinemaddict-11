410fa275c6c9662310f8ecc0a38ed7effdfaa1e9
George Bukhtaty <gbukhtatyy@gmail.com>
2020-05-16 23:51:47 +0300

    WIP

--- src/api/index.js
+++ src/api/index.js
@@ -21,33 +21,109 @@ export default class Api {
     this._authorization = authorization;
   }
 
+  //#region movies
+
+  /**
+   * Получение информации о фильмах
+   * @return {Array}
+   * @return {Promise}
+   */
   getMovies() {
     return this._load(`movies`)
       .then((response) => response.json())
       .then(Movie.parseMovies);
   }
 
-  getComments() {
-    return this._load({url: `comments`})
+  /**
+   * Выставление рейтинга, добавление в избранное.
+   *
+   * @param {number} filmId
+   * @param {array} userDetails
+   * @return {Promise}
+   */
+  updateMovie(filmId, userDetails) {
+    return this._load({
+      url: `movies/${filmId}`,
+      method: Method.PUT,
+      body: JSON.stringify(userDetails),
+      headers: new Headers({'Content-Type': `application/json`})
+    })
+      .then((response) => response.json())
+      .then(Movie.parseMovies);
+  }
+
+  //#endregion
+
+  //#region comments
+
+  /**
+   * Получение комментариев к фильму.
+   * GET /comments/:film_id
+   *
+   * @param {number} filmId
+   * @return {Promise}
+   */
+  getComments(filmId) {
+    return this._load({url: `comments/${filmId}`})
       .then((response) => response.json());
   }
 
-  updateMovie(id, data){
-
+  /**
+   * Создание нового комментария к фильму
+   * POST /comments/:film_id
+   *
+   * @param {number} filmId
+   * @param {array} comment
+   * @return {Promise}
+   */
+  createComment(filmId, comment) {
+    return this._load({
+      url: `comments/${filmId}`,
+      method: Method.PUT,
+      body: JSON.stringify(comment),
+      headers: new Headers({'Content-Type': `application/json`})
+    })
+      .then((response) => response.json())
+      .then(Movie.parseMovies);
   }
 
-  createComment(comment){
-
+  /**
+   * Удаление существующего комментария.
+   * DELETE /comments/:comment_id
+   *
+   * @param {number} commentId
+   * @return {Promise}
+   */
+  deleteComment(commentId) {
+    return this._load({url: `comments/${commentId}`, method: Method.DELETE});
   }
 
-  updateComment(id, data){
+  //#endregion
 
+  /**
+   * Синхронизация фильмов
+   *
+   * @param {array} movies
+   * @return {Promise}
+   */
+  sync(movies) {
+    return this._load({
+      url: `movies/sync`,
+      method: Method.POST,
+      body: JSON.stringify(movies),
+      headers: new Headers({'Content-Type': `application/json`})
+    })
+      .then((response) => response.json());
   }
 
-  deleteComment(id){
-
-  }
-
+  /**
+   *
+   * @param {string} url
+   * @param {Method} method
+   * @param {array} body
+   * @param {array} headers
+   * @return {Promise}
+   */
   _load(url, method = Method.GET, body = null, headers = new Headers()) {
     headers.append(`Authorization`, this._authorization);
 
